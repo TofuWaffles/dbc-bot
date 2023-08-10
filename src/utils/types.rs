f7f56b6ee4dcb52700b6185b8efaa72da0abb740
Wolfus20 <silvallywish@gmail.com>
2023-08-10 18:27:58 +0800

    Add comments

--- src/utils/types.rs
+++ src/utils/types.rs
@@ -1,3 +1,8 @@
+/*
+This file contains various common type definitions or type aliases that are used throughout the bot.
+For now, these are mainly used for type annotations
+*/
+
 pub struct Data {}
-pub type Error = Box<dyn std::error::Error + Send + Sync>;
-pub type Context<'a> = poise::Context<'a, Data, Error>;
+pub type Error = Box<dyn std::error::Error + Send + Sync>;  // Error type used by Poise
+pub type Context<'a> = poise::Context<'a, Data, Error>;     // Context type as defined by poise
