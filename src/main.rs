373f2bdec70dc7522a721557a0f95d70c4529a7e
Darkness <kizluxury0506@gmail.com>
2023-08-24 22:22:34 +0700

    likewise, complete player command

--- src/main.rs
+++ src/main.rs
@@ -6,16 +6,16 @@ TODO:
 mod bracket_tournament;
 mod commands;
 mod self_role;
+mod utils;
 
 use dashmap::DashMap;
-use poise::serenity_prelude::InteractionType;
-use poise::serenity_prelude::{self as serenity, GatewayIntents, MessageComponentInteraction};
-use poise::Event;
+use poise::serenity_prelude::{self as serenity, GatewayIntents, MessageComponentInteraction, InteractionType};
+
 
 // This data struct is used to pass data (such as the db_pool) to the context object
 pub struct Data {
-    db_pool: sqlx::PgPool,
-    self_role_messages: DashMap<i64, self_role::SelfRoleMessage>, // Required for the self_role module
+    // db_pool: sqlx::PgPool,
+    // self_role_messages: DashMap<i64, self_role::SelfRoleMessage>, // Required for the self_role module
 }
 pub type Error = Box<dyn std::error::Error + Send + Sync>;
 pub type Context<'a> = poise::Context<'a, Data, Error>;
@@ -25,97 +25,28 @@ async fn main() {
     // Load the environment variable from the .env file
     dotenv::dotenv().expect("Unable to load the .env file. Check if it has been created.");
 
-    if let Err(e) = run().await {
-        println!("Error trying to run the bot: {}", e);
-    }
-}
+    let token = std::env::var("DISCORD_TOKEN")
+        .expect("DISCORD_TOKEN is not set. Set it as an environment variable.");
 
-async fn run() -> Result<(), Error> {
-    // The list of commands goes here
-    let options = poise::FrameworkOptions {
-        commands: vec![ping::ping()],
-        event_handler: |ctx, event, _framework, data| {
-            Box::pin(async move {
-                match event {
-                    Event::Ready { data_about_bot } => {
-                        println!("{} is connected!", data_about_bot.user.name);
-                    }
-                    Event::InteractionCreate { interaction } => match interaction {
-                        serenity::Interaction::MessageComponent(message_component_interaction) => {
-                            match message_component_interaction.data.component_type {
-                                // We exhaustively check the specific interaction type so that we don't have to do it inside every function
-                                serenity::ComponentType::Button => todo!(),
-                                _ => (),
-                            }
-                        }
-                        _ => (),
-                    },
-                   _ => (),
-                }
-
-                Ok(())
-            })
-        },
-        ..Default::default()
-    };
-
-    let db_pool = sqlx::postgres::PgPoolOptions::new()
-        .max_connections(10)
-        .connect(
-            &std::env::var("DATABASE_URL").expect("Missing DATABASE_URL environment variable."),
-        )
-        .await?;
-
-    sqlx::migrate!("./migrations").run(&db_pool).await?;
-
-    // We want to load the messages into a hashmap for quick lookup in the self_role event handler
-    let self_role_messages = DashMap::<i64, self_role::SelfRoleMessage>::new();
-
-    for msg in sqlx::query_as!(
-        self_role::SelfRoleMessage,
-        "SELECT message_id, guild_id, role_id, ping_channel_id FROM self_role_message;"
-    )
-    .fetch_all(&db_pool)
-    .await?
-    {
-        self_role_messages.insert(msg.message_id, msg);
-    }
+    // A list of commands to register. Remember to add the function for the command in this vec, otherwise it won't appear in the command list.
+    // Might be better to find a more scalable and flexible solution down the line.
+    let commands = vec![commands::ping::ping(), commands::player::player(), commands::battle_log::log()];
 
     let framework = poise::Framework::builder()
-        .token(std::env::var("DISCORD_TOKEN").expect("Missing DISCORD_TOKEN environment variable."))
-        .setup(move |ctx, _ready, framework| {
-            Box::pin(async move {
-                ctx.set_activity(serenity::Activity::playing("Discord Brawl Cup"))
-                    .await;
-                poise::builtins::register_globally(ctx, &framework.options().commands).await?;
-
-                Ok(Data {
-                    db_pool,
-                    self_role_messages,
-                })
-            })
+        .options(poise::FrameworkOptions {
+            commands: commands,
+            ..Default::default()
         })
-        .initialize_owners(true)
-        .options(options)
-        .intents(
-            GatewayIntents::non_privileged()
-                | GatewayIntents::MESSAGE_CONTENT
-                | GatewayIntents::GUILD_MEMBERS,
-        )
-        .build()
-        .await?;
+        .token(token)
+        .intents(serenity::GatewayIntents::non_privileged())
+        .setup(|ctx, _ready, framework| {
+            Box::pin(async move {
+                poise::builtins::register_globally(ctx, &framework.options().commands).await?;
+                Ok(Data {})
+            })
+        });
 
-    let shard_manager = framework.shard_manager().clone();
-
-    tokio::spawn(async move {
-        tokio::signal::ctrl_c()
-            .await
-            .expect("Could not register the ctrl+c handler");
-        shard_manager.lock().await.shutdown_all().await;
-    });
-
-    println!("Starting the bot...");
-    framework.start().await?;
-
-    Ok(())
+    println!("The bot is starting...");
+    framework.run().await.unwrap();
 }
+
