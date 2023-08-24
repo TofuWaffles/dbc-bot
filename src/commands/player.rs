48fe04985fa6eb2ae7e400e67430297059a4fcd2
Wolfus20 <silvallywish@gmail.com>
2023-08-24 17:06:29 +0800

    Regenerate cargo lock

--- src/commands/player.rs
+++ src/commands/player.rs
@@ -1,5 +1,6 @@
 use poise::CreateReply;
-use poise::serenity_prelude::{Embed, CreateEmbed};
+use poise::serenity_prelude::CreateEmbed;
+
 
 use crate::utils::types;
 use crate::bracket_tournament::player::Player;
@@ -8,30 +9,29 @@ use crate::bracket_tournament::player::Player;
 #[poise::command(slash_command, prefix_command)]
 pub async fn player(
   ctx: types::Context<'_>, 
-  #[description = "Put your tag here (without #)" ] tag: String) -> Result<(), types::Error> {
+  #[description = "Put your tag here (without #)" ] tag: String) 
+-> Result<(), types::Error>{
   let player = Player::new(&tag).await;
-  let embed = CreateReply{
-    content: None,
-    attachments: vec![],
-    allowed_mentions: None,
-    components: None,
-    ephemeral: false,
-    reply: false,
-    embeds: vec![CreateEmbed()
-      .title(format!("**{}({})**",player.name, player.tag))
-      .fields(vec![
-        ("Trophies", format!("{}", player.trophies), true),
-        ("Highest Trophies", format!("{}", player.highest_trophies), true),
-        ("3v3 Victories", format!("{}", player.victories_3v3), true),
-        ("Solo Victories", format!("{}", player.solo_victories), true),
-        ("Duo Victories", format!("{}", player.duo_victories), true),
-        ("Best Robo Rumble Time", format!("{}", player.best_robo_rumble_time), true),
-        ("Club", format!("{}({})", player.club.name, player.club.tag), true),
-      ])
-    ],
-  };
-  let response = |embed: &mut CreateReply| embed;
-  ctx.send(response).await?;
+  let mut embed = CreateEmbed::default();
+
+  embed
+    .title(format!("**{}({})**",player.name, player.tag))
+    .fields(vec![
+      ("Trophies", format!("{}", player.trophies), true),
+      ("Highest Trophies", format!("{}", player.highest_trophies), true),
+      ("3v3 Victories", format!("{}", player.victories_3v3), true),
+      ("Solo Victories", format!("{}", player.solo_victories), true),
+      ("Duo Victories", format!("{}", player.duo_victories), true),
+      ("Best Robo Rumble Time", format!("{}", player.best_robo_rumble_time), true),
+      ("Club", format!("{}({})", player.club.name, player.club.tag), true),
+    ]);
+  let mut reply = CreateReply::default();
+  reply.embeds.push(embed);
+
+  let builder =  |reply: &mut CreateReply| reply;
+  
+
+  ctx.send(builder).await?;
   Ok(())
 }
 
