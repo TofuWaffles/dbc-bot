cdda5ee8d38bf2a16a79bd2126c80af26832a8ed
NightMat01 <48952054+NightMat01@users.noreply.github.com>
2023-08-25 12:36:53 +0100

    add db handling (inital commit for that)
    imports are not finished and need to be continued, client uses a db handler for pooling and db retrieval

--- src/utils/db_handling.rs
+++ src/utils/db_handling.rs
@@ -1,5 +1,7 @@
-use mongodb::{bson::doc, options::ClientOptions, options::FindOptions, Client};
 use serde::{Deserialize, Serialize};
+use mongodb::{Client, bson::doc, options::ClientOptions, options::FindOptions};
+use futures::stream::TryStreamExt;
+use crate::{Context, Error};
 
 #[derive(Debug, Serialize, Deserialize)]
 struct SelfRoles {
@@ -14,19 +16,18 @@ struct DbPooling {
 
 impl DbPooling {
     #[tokio::main]
-    pub async fn new(uri: String) -> Result<Client, Box<dyn std::error::Error + Send + Sync>> {
+    pub async fn new(uri: &'static str) -> Result<Client, Box<dyn std::error::Error + Send + Sync>> {
         let client_options = ClientOptions::parse(&uri).await?;
         Ok(Client::with_options(client_options)?)
     }
 }
 
-async fn retrieve_self_roles_data(
-    args: &[&str],
-) -> Result<String, Box<dyn std::error::Error + Send + Sync>> {
-    let db_uri = std::env::var("DATABASE_URL")
-        .expect("DATABASE_URL is not set. Set it as an environment variable.");
+async fn retrieve_self_roles_data(args: &[&str]) -> Result<String, Box<dyn std::error::Error + Send + Sync>> {
 
-    let client = DbPooling::new(db_uri).unwrap();
+    let db_uri = std::env::var("DISCORD_TOKEN")
+    .expect("DISCORD_TOKEN is not set. Set it as an environment variable.");
+
+    let client = DbPooling::new(&db_uri).unwrap();
 
     let db = client.database("DBC-bot");
 
@@ -34,9 +35,7 @@ async fn retrieve_self_roles_data(
 
     let filter = doc! { "guildId": args[0] };
 
-    let find_options = FindOptions::builder()
-        .sort(doc! { "user": args[1], "option": args[2] })
-        .build();
+    let find_options = FindOptions::builder().sort(doc! { "user": args[1], "option": args[2] }).build();
 
     let mut cursor = collection.find(filter, find_options).await?;
 
--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -3,4 +3,3 @@ pub mod embed_color;
 pub mod api;
 pub mod db_handling;
 pub mod api;
-pub mod mode;
