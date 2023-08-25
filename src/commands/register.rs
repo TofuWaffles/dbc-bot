9201f4bd0ad20823eb3ddfc7cee7a7f450fa0c5a
NightMat01 <48952054+NightMat01@users.noreply.github.com>
2023-08-25 20:38:13 +0100

    fixed register and player info retrieval commands

--- src/commands/register.rs
+++ src/commands/register.rs
@@ -1,9 +1,9 @@
-use crate::bracket_tournament::player::{Player};
+use crate::bracket_tournament::player::PlayerDB;
 use crate::{Context, Error};
 
 #[poise::command(slash_command, prefix_command)]
 pub async fn register(ctx: Context<'_>, #[description = "Put your tag here (without #)" ] tag: String) -> Result<(), Error>{
-    let player_register_data = Player::new(&tag).await;
+    let player_register_data = PlayerDB::new(&tag, &ctx.author().id).await;
     let player_data = ctx.data().client.database("DBC-bot").collection("PlayerDB");
     
     player_data.insert_one(player_register_data, None).await?;
