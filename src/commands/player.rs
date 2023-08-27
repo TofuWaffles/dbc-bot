1046c9a82691c9351d4660df5df0dc085abb582c
Darkness <kizluxury0506@gmail.com>
2023-08-27 21:39:20 +0700

    pull from bracket

--- src/commands/player.rs
+++ src/commands/player.rs
@@ -11,7 +11,6 @@ pub async fn player(
     let endpoint = api::api_handlers::get_api_link("player", &tag);
     match api::api_handlers::request(&endpoint).await {
         Ok(player) => {
-            println!("{}", json);
             ctx.send(|s| {
                 s.content("".to_string())
                     .reply(true)
