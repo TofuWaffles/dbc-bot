e80d5471840f00cffd8d76264c70f9f15bb248d0
Wolfus20 <silvallywish@gmail.com>
2023-08-27 22:41:55 +0800

    Nuke sqlx's migrations

--- migrations/20230817080233_self_role.sql
+++
@@ -1,8 +0,0 @@
--- Add migration script here
-
-CREATE TABLE IF NOT EXISTS self_role_message (
-    message_id BIGINT PRIMARY KEY,
-    guild_id BIGINT NOT NULL,
-    role_id BIGINT NOT NULL,
-    ping_channel_id BIGINT NOT NULL
-)
