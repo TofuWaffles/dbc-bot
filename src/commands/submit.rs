dcfb0ecc410f587e27ed790406956a18bbf2d5b9
Darkness <kizluxury0506@gmail.com>
2023-08-28 21:39:41 +0700

    reformat

--- src/commands/submit.rs
+++ src/commands/submit.rs
@@ -10,25 +10,26 @@ const MAX_BRAWLER_LEVEL: i32 = 11;
 #[poise::command(slash_command, guild_only)]
 pub async fn submit(ctx: Context<'_>) -> Result<(), Error> {
     let tag = "RR82U9J0".to_string();
-    let log = api_handlers::request(&api_handlers::get_api_link("battle_log", &tag))
+    let raw_logs = api_handlers::request(&api_handlers::get_api_link("battle_log", &tag))
         .await
         .unwrap();
+    let logs = raw_logs["items"].as_array().unwrap();
     let team = sample_json::match_json();
     println!("Team: {}", team);
-    let player1 = team[0]["name"].to_string().strip_quote();
+    let player1 = team[0]["tag"].to_string().strip_quote();
     println!("Player 1: {}", player1);
-    let player2 = team[1]["name"].to_string().strip_quote();
+    let player2 = team[1]["tag"].to_string().strip_quote();
     println!("Player 2: {}", player2);
     let mut index = 0;
     let mut result = "".to_string();
-    for log_index in 0..=30 {
-        let mode = log["items"][log_index]["event"]["mode"]
+    for (log_index, log_value) in logs.iter().enumerate() {
+        let mode = log_value["event"]["mode"]
             .to_string()
             .strip_quote();
-        let player1_log = log["items"][log_index]["battle"]["teams"][0][0]["name"]
+        let player1_log = log_value["battle"]["teams"][0][0]["tag"]
             .to_string()
             .strip_quote();
-        let player2_log = log["items"][log_index]["battle"]["teams"][1][0]["name"]
+        let player2_log = log_value["battle"]["teams"][1][0]["tag"]
             .to_string()
             .strip_quote();
         //Number of logs
@@ -36,56 +37,56 @@ pub async fn submit(ctx: Context<'_>) -> Result<(), Error> {
             && (player1 == player1_log || player1 == player2_log)
             && (player2 == player1_log || player2 == player2_log)
         {
-            // println!("Found the log for player {} and {}", player1, player2);
-            result = log["items"][log_index]["battle"]["result"].to_string();
-            break;
+            // println!("Found the logs for player {} and {}", player1, player2);
+            result = logs[log_index]["battle"]["result"].to_string();
+
         }
         index += 1;
     }
-        // println!(
-        //     "Battle log {}\nMode: {}\nPlayer 1: {}\nPlayer2: {}",
-        //     log_index,
-        //     log["items"][log_index]["event"]["mode"]
-        //         .to_string()
-        //         .strip_quote(),
-        //     log["items"][log_index]["battle"]["teams"][0][0]["name"]
-        //         .to_string()
-        //         .strip_quote(),
-        //     log["items"][log_index]["battle"]["teams"][1][0]["name"]
-        //         .to_string()
-        //         .strip_quote()
-        // );
+    // println!(
+    //     "Battle logs {}\nMode: {}\nPlayer 1: {}\nPlayer2: {}",
+    //     log_index,
+    //     logs[log_index]["event"]["mode"]
+    //         .to_string()
+    //         .strip_quote(),
+    //     logs[log_index]["battle"]["teams"][0][0]["name"]
+    //         .to_string()
+    //         .strip_quote(),
+    //     logs[log_index]["battle"]["teams"][1][0]["name"]
+    //         .to_string()
+    //         .strip_quote()
+    // );
 
     if index <= 21 {
         ctx.send(|s| {
             s.content("".to_string()).reply(true).embed(|e| {
                 e.author(|a| a.name(ctx.author().name.clone()))
-                    .title("Battle log result")
+                    .title("Battle logs result")
                     .color(get_color(result.clone()))
-                    .thumbnail(get_mode_icon(&log["items"][index]["event"]["mode"])) // why it no shows?
+                    .thumbnail(get_mode_icon(&logs[index]["event"]["mode"])) // why it no shows?
                     .field(
                         "Battle Time",
-                        log["items"][index]["battleTime"].to_string(),
+                        logs[index]["battleTime"].to_string(),
                         false,
                     )
                     .fields(vec![
                         (
                             "Mode",
-                            log["items"][index]["event"]["mode"]
+                            logs[index]["event"]["mode"]
                                 .to_string()
                                 .strip_quote(),
                             true,
                         ),
                         (
                             "Map",
-                            log["items"][index]["event"]["map"]
+                            logs[index]["event"]["map"]
                                 .to_string()
                                 .strip_quote(),
                             true,
                         ),
                         (
                             "Duration",
-                            log["items"][index]["battle"]["duration"]
+                            logs[index]["battle"]["duration"]
                                 .to_string()
                                 .strip_quote()
                                 + "s",
@@ -93,14 +94,14 @@ pub async fn submit(ctx: Context<'_>) -> Result<(), Error> {
                         ),
                         (
                             "Game",
-                            log["items"][index]["battle"]["type"]
+                            logs[index]["battle"]["type"]
                                 .to_string()
                                 .strip_quote(),
                             true,
                         ),
                         (
                             "Trophy Change",
-                            log["items"][index]["battle"]["trophyChange"]
+                            logs[index]["battle"]["trophyChange"]
                                 .to_string()
                                 .strip_quote(),
                             true,
@@ -110,12 +111,12 @@ pub async fn submit(ctx: Context<'_>) -> Result<(), Error> {
                     .field("===============================".to_string(), "", false)
                     .fields(vec![
                         (
-                            log["items"][index]["battle"]["teams"][0][0]["name"]
+                            logs[index]["battle"]["teams"][0][0]["name"]
                                 .to_string()
                                 .strip_quote(),
                             format!(
                                 "Brawler: {}\nPower: {}",
-                                &log["items"][index]["battle"]["teams"][0][0]["brawler"]["name"]
+                                &logs[index]["battle"]["teams"][0][0]["brawler"]["name"]
                                     .to_string()
                                     .strip_quote(),
                                 MAX_BRAWLER_LEVEL
@@ -124,12 +125,12 @@ pub async fn submit(ctx: Context<'_>) -> Result<(), Error> {
                         ),
                         ("VS".to_string(), "".to_string(), true),
                         (
-                            log["items"][index]["battle"]["teams"][1][0]["name"]
+                            logs[index]["battle"]["teams"][1][0]["name"]
                                 .to_string()
                                 .strip_quote(),
                             format!(
                                 "Brawler: {}\nPower: {}",
-                                &log["items"][index]["battle"]["teams"][1][0]["brawler"]["name"]
+                                &logs[index]["battle"]["teams"][1][0]["brawler"]["name"]
                                     .to_string()
                                     .strip_quote(),
                                 MAX_BRAWLER_LEVEL
@@ -147,7 +148,7 @@ pub async fn submit(ctx: Context<'_>) -> Result<(), Error> {
             .reply(true)
             .ephemeral(false)
             .embed(|e| {
-                e.title("No battle log found (yet?)")
+                e.title("No battle logs found (yet?)")
                     .description("As the result is recorded nearly in real-time, please try again later. It may take up to 30 minutes for a new battle to appear in the battlelog")
             })
     })
