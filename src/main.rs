8676c2066e54de7eea1d47af8aaeae82cdb8b6ad
Wolfus20 <silvallywish@gmail.com>
2023-08-29 19:35:42 +0800

    Add more required collections and other minor changes

--- src/main.rs
+++ src/main.rs
@@ -27,6 +27,7 @@ use tracing_subscriber::{filter, prelude::*};
 // Rest of your code here
 
 // This data struct is used to pass data (such as the db_pool) to the context object
+#[derive(Debug)]
 pub struct Data {
     database: mongodb::Database,
     self_role_messages: DashMap<i64, self_role::SelfRoleMessage>, // Required for the self_role module
@@ -138,7 +139,7 @@ async fn run() -> Result<(), Error> {
         .setup(|ctx, _ready, framework| {
             Box::pin(async move {
                 poise::builtins::register_globally(ctx, &framework.options().commands).await?;
-                let mut self_role_data = database 
+                let mut self_role_data = database
                     .collection::<self_role::SelfRoleMessage>("SelfRoleMessage")
                     .find(None, None)
                     .await
@@ -200,9 +201,21 @@ async fn prepare_database() -> Result<Database, Error> {
 
     let database = Client::with_options(options)?.database("DBC");
 
-    // We want to preload the self role messages, which is why we create this collection if it does not exist
+    let required_collections = vec![
+        "Player",
+        "SelfRoleMessage",
+        "TournamentStatus",
+        "BracketPair",
+    ];
+
+    // We want to preload some of these collections, which is why we create this collection if it does not exist
     // Errors if the DB already exists and skips creation
-    database.create_collection("SelfRoleMessage", None).await.unwrap_or_else(|e| {info!("{:?}", e)});
+    for collection in required_collections {
+        database
+            .create_collection(collection, None)
+            .await
+            .unwrap_or_else(|e| info!("{:?}", e));
+    }
 
     info!("Database prepared successfully!");
 
