d8f3225d7232af8d234c4a280f5f5c05e12212a3
NightMat01 <nightmat01@gmail.com>
2023-09-01 08:08:49 +0100

    update registration id before aggregation

--- src/commands/starttournament.rs
+++ src/commands/starttournament.rs
@@ -1,5 +1,6 @@
 use crate::misc::Region;
 use crate::{Context, Error};
+use futures::TryStreamExt;
 use mongodb::{
     bson::{doc, Document},
     options::AggregateOptions,
@@ -14,6 +15,24 @@ use strum::IntoEnumIterator;
 )]
 pub async fn start_tournament(ctx: Context<'_>) -> Result<(), Error> {
     let collection: Collection<Document> = ctx.data().database.collection("Player");
+
+    // Update registration IDs for each document
+    let mut player_cursor = collection.find(None, None).await?;
+    let mut count = 1;
+
+    while let Ok(Some(mut document)) = player_cursor.try_next().await {
+        let registration_id = count;
+        count += 1;
+
+        document.insert("registration_id", registration_id);
+
+        collection.replace_one(
+            doc! { "_id": document.get_object_id("_id")? },
+            document,
+            None,
+        ).await?;
+    }
+
     // Filter out by region and put them into a new collection
     for region in Region::iter() {
         let pipeline: Vec<Document> = vec![
