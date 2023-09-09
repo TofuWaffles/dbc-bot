acb271283c92b2a4c1eaa23383a507e4839efba2
Darkness <101615478+Darkiee12@users.noreply.github.com>
2023-09-09 18:54:35 +0700

    Update ping.rs

--- src/commands/ping.rs
+++ src/commands/ping.rs
@@ -3,7 +3,7 @@ use crate::{Context, Error};
 /// Ping the bot and possibly get a response, probably, maybe, maybe not?
 #[poise::command(slash_command, guild_only)]
 pub async fn ping(ctx: Context<'_>) -> Result<(), Error> {
-    let response = "Pong".to_owned();
+    let response = "Pong".to_string();
     ctx.say(response).await?;
     Ok(())
 }
