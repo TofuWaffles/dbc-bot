1a10236d7ec060c93ccf7d23c7a96dfa85bd24fd
Darkness <kizluxury0506@gmail.com>
2024-02-17 03:52:07 +0700

    FINALLY CERT HANDLE DONE

--- src/brawlstars/api.rs
+++ src/brawlstars/api.rs
@@ -1,5 +1,5 @@
-use std::{fs::File, io::Read};
-
+use reqwest::StatusCode;
+use tracing::info;
 use crate::Error;
 use poise::serenity_prelude::json::Value;
 use reqwest;
@@ -10,12 +10,12 @@ pub enum APIResult {
     APIError(u16),
 }
 fn get_player(player_tag: &str) -> String {
-    format!("https://api.brawlstars.com/v1/players/%23{}", player_tag)
+    format!("https://bsproxy.royaleapi.dev/v1/players/%23{}", player_tag)
 }
 
 fn get_battle_log(player_tag: &str) -> String {
     format!(
-        "https://api.brawlstars.com/v1/players/%23{}/battlelog",
+        "https://bsproxy.royaleapi.dev/v1/players/%23{}/battlelog",
         player_tag
     )
 }
@@ -32,23 +32,25 @@ pub async fn request(option: &str, tag: &str) -> Result<APIResult, Error> {
     };
 
     let token = std::env::var("BRAWL_STARS_TOKEN").expect("Brawl Stars API token not found.");
-    let mut buf = Vec::new();
-    File::open("cacert.pem")?.read_to_end(&mut buf)?;
-    let cert = reqwest::Certificate::from_pem(&buf)?;
-    let builder = reqwest::Client::builder().add_root_certificate(cert);
+
     
-    let response = builder.build().unwrap()
+    let response = reqwest::Client::new()
         .get(endpoint)
         .header("Authorization", format!("Bearer {}", token))
         .send()
         .await?;
 
-    if response.status().is_success() {
-        let data: Value = response.json().await?;
-        Ok(APIResult::Successful(data))
-    } else if response.status().is_client_error() {
-        Ok(APIResult::NotFound(response.status().as_u16()))
-    } else {
-        Ok(APIResult::APIError(response.status().as_u16()))
-    }
+        match response.status() {
+            StatusCode::OK => {
+                let data: Value = response.json().await?;
+                Ok(APIResult::Successful(data))
+            }
+            StatusCode::NOT_FOUND => {
+                Ok(APIResult::NotFound(response.status().as_u16()))
+            }
+            _ => {
+                info!("API error {}", response.status());
+                Ok(APIResult::APIError(response.status().as_u16()))
+            }
+        }
 }
